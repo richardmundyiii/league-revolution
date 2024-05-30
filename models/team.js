9397f20bb608f4e7f6433725ba451a4d75b3ccc2
Richard Mundy <mundy.richard@icloud.com>
2024-05-30 13:25:21 -0700

    add Team DB

---
+++ models/team.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const teamSchema = new Schema({
+  name: String,
+  division: { type: mongoose.Schema.Types.ObjectId, ref: "Division" },
+  venue: { type: mongoose.Schema.Types.ObjectId, ref: "Venue" },
+  players: [{ type: mongoose.Schema.Types.ObjectId, ref: "Player" }],
+  stats: {
+    wins: Number,
+    losses: Number,
+    totalLegsWon: Numner,
+    totalLegsLost: Number,
+    teamGamesWon: Number,
+    teamGamesLost: Number,
+  },
+});
+
+module.exports = mongoose.model("Team", teamSchema);
