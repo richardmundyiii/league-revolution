e9c03c07a2ed423bf8ae89d64c6e6ffb8f00ffce
Richard Mundy <mundy.richard@icloud.com>
2024-05-31 14:18:27 -0700

    add team to reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import seasonReducer from "./seasonReducer";
 import divisionReducer from "./divisionReducer";
+import teamReducer from "./teamReducer";
 
 const rootReducer = combineReducers({
   season: seasonReducer,
   division: divisionReducer,
+  team: teamReducer,
 });
 
 export default rootReducer;
